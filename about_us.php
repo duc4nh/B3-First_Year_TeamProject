c80c37536f3cf254449acd8f64695963c999cdca
Duc Anh <im_online1201@yahoo.com>
2013-03-02 14:05:56 +0000

    created about us files

---
+++ about_us.php
@@ -0,0 +1,30 @@
+<!-- header and menu left -->
+<?php include('header_menuleft.php'); ?>
+  
+  <!-- Main body for page -->
+  <div id="body">
+    <div id="top_search">
+      <input class="search_box" type="text"><input class="search_button" value="SEARCH" type="submit"/>
+    </div>
+  
+  </br>
+  <h2>About Us</h2>
+  <p>Group B3 - Class of 2015 - University Of Manchester</p>
+  </br>
+  
+  <ul>
+    <li>Karolis Skripka
+    <li>Duc Anh
+    <li>Ali Safarli
+    <li>Vlad Berianu
+    <li>Eugene Varghin
+    <li>Giorgos Taf
+    <li>Lorin Patrick Nicolae
+  </ul>
+  
+  </div>
+
+  <!-- Main body for page ends -->
+  
+<!-- Footer information -->
+<?php include('footer.php'); ?>
