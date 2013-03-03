7478552527ba8931d9ef2469ac514f00777d7672
vladut <berianuvlad@gmail.com>
2013-03-03 21:01:50 +0000

    I have added header and footer to the page and the search bar

--- login.php
+++ login.php
@@ -1,4 +1,9 @@
 <?php
+include('header_menuleft.php'); 
+echo "<div id='body'>
+    <div id='top_search'>
+      <input class='search_box' type='text'><input class='search_button' value='SEARCH' type='submit'/>
+    </div>" ;
 session_start();
 $email = $_POST['email'];
 $password = $_POST['password'];
@@ -16,17 +21,24 @@ if($email && $password)
     {
     	 $dbemail = $row['email'];
     	 $dbpassword = $row['password'];
-         $id = $row['user_id'];
-         $name = $row['name'];
-         $last_name = $row['last_name'];
+       $user_id = $row['user_id'];
+       $name = $row['name'];
+       $last_name = $row['last_name'];
+       $description = $row['description'];
+       $picture = $row['picture'];
+       if($picture == NULL)
+           $picture = 'http://www.worldofchemicals.com/Woclite/tmp/chem/no_image.gif';
     }
     if ($email == $dbemail && $password == $dbpassword)
     {
       $_SESSION['email'] = $email;
-      $_SESSION['id'] = $id;
+      $_SESSION['user_id'] = $user_id;
       $_SESSION['name'] = $name;
       $_SESSION['last_name'] = $last_name;
-      echo "You are logged in! <a href='userpage.php?id=".$id."'>Click here </a> to go to your profile! ";
+      $_SESSION['description'] = $description;
+      $_SESSION['picture'] = $picture;
+      
+      echo "<br />You are logged in! <a href='userpage.php?id=".$user_id."'>Click here </a> to go to your profile! ";
     }
     else
     {
@@ -44,4 +56,6 @@ else
 {
   die("Please enter email and password!");
 }
+echo "</div>";
+include('footer.php');
 ?>
