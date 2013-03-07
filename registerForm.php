4b23a28e5a2054a31513d875860d55bb24ce8528
vladut <berianuvlad@gmail.com>
2013-03-07 12:15:42 +0000

    Fixed some get/post things. Needs design!

--- registerForm.php
+++ registerForm.php
@@ -6,42 +6,26 @@
     <div id="top_search">
       <input class="search_box" type="text"><input class="search_button" value="SEARCH" type="submit"/>
     
-        </br>
-        </br>
-        <h3>Welcome to the registration page</h3></br>
 
-        Please fill the blanks for creating new account</br>
-        </br>
-      <div id="register_box">
-        <form name="input" action="register.php" method="get">  
-          <table width="100%" border="0" cellpadding="0" cellspacing="2">
-            <tr>
-            <td width="1%">Username: </td>
-              <td><input type="text" name="regname"></td>
-            </tr><tr></tr>
-            <tr>
-              <td width="1%"> E-mail adress: </td>
-              <td> <input type="text" name="regemail"></td>
-            </tr><tr></tr>
-            <tr>
-              <td width="1%">Password:</td>
-              <td><input type="password" name="regpass1"></td>
-            </tr><tr></tr>
-            <tr>
-              <td width="1%">Re-type password: </td>
-              <td><input type="password" name="regpass2"></td>
-            <tr>
-              <td>&nbsp;</td>
-              <td><button type="submit" />     Submit      </button></td>
-            </tr><tr></tr>
-          </table>
-        </form>
-      </div>
+Welcome to the registration page</br>
+
+Please fill the blanks for creating new account</br>
+
+
+<form name="input" action="register.php" method="post">
+
+E-mail adress: <input type="text" name="regemail"><br>
+
+Password: <input type="text" name="regpass1"><br>
+Re-type password: <input type="text" name="regpass2"><br>
+<input type="submit" value="Submit">
+</form>
+   
     </div>
    
   </div>
 
   <!-- Main body for page ends -->
-    
+  
 <!-- Footer information -->
 <?php include('footer.php'); ?>
