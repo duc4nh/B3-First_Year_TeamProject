bc0940789b33f686929cddf7b5e50d259823ee6f
vladut <berianuvlad@gmail.com>
2013-03-13 13:17:24 +0000

    i have made a slider for the first

---
+++ slider.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html class="no-js">
+
+	<head>
+		<meta charset="utf-8"/>
+
+		 
+		<!--[if lt IE 9]>
+			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<link rel="stylesheet" media="all" href="cs/style.css"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<!-- Adding "maximum-scale=1" fixes the Mobile Safari auto-zoom bug: http://filamentgroup.com/examples/iosScaleBug/ -->		
+				
+		<!-- JS -->
+		<script src="js/jquery-1.7.1.min.js"></script>
+		<script src="js/custom.js"></script>
+		
+		<script src="js/css3-mediaqueries.js"></script>
+		<script src="js/jquery.columnizer.min.js"></script>
+		
+		
+		<!-- Lof slider -->
+		<script src="js/jquery.easing.js"></script>
+		<script src="js/lof-slider.js"></script>
+		<link rel="stylesheet" href="css/lof-slider.css" media="all"  /> 
+		<!-- ENDS slider -->
+	
+		
+		
+
+		<!-- JCarousel -->
+		<script type="text/javascript" src="js/jquery.jcarousel.min.js"></script>
+		<link rel="stylesheet" media="screen" href="css/carousel.css" /> 
+		<!-- ENDS JCarousel -->
+		
+
+		<!-- SKIN -->
+		<link rel="stylesheet" media="all" href="css/skin.css"/>
+		
+		<!-- flexslider -->
+		<link rel="stylesheet" href="css/flexslider.css" >
+		
+
+	</head>
+	
+				<!-- SLIDER -->				
+				<div id="home-slider" class="lof-slidecontent">
+					
+					<div class="preload"></div>
+					
+					<!-- slider content --> 
+					<div class="main-slider-content" >
+					<ul class="sliders-wrap-inner">
+					    <li>
+					          <img src="images/slides/01.jpg" title="" alt="alt" />           
+					          <div class="slider-description">
+					            <h4>Who are we</h4>
+					            <p>We are first year students, group B3 from University of Manchester, School of Computer Science. 
+					            </p>
+					         </div>
+					    </li>
+					    
+					    <li>
+					          <img src="images/slides/02.jpg" title="" alt="alt" />           
+					          <div class="slider-description">
+					            <h4>What we do</h4>
+					            <p>The current website is our First Year Tem Project developed during 6 months of work. 
+					            </p>
+					         </div>
+					    </li>
+					    
+					    <li>
+					          <img src="images/slides/03.jpg" title="" alt="alt" />           
+					          <div class="slider-description">
+					            <h4>What is our project about</h4>
+					            <p>MyUniTrader is a website developed for students which enables them to trade books, clothes, devices etc. 
+					            </p>
+					         </div>
+					    </li>
+					    
+					    <li>
+					          <img src="images/slides/04.jpg" title="" alt="alt" />           
+					          <div class="slider-description">
+					            <h4>Contact</h4>
+					            <p>If you have complaints or suggestions, please contact us:<br><br>
+
+					            	-Berianu Vlad: berianuvlad@gmail.com<br>
+					            	-Duc Anh: example@example.com<br>
+					            	-Karolis Skripka: example@example.com<br>
+					            	-Eugene Varghin: example@example.com<br>
+					            	-Ali Safarli: example@example.com<br>
+					            	-Georgios Tzamousis:example@example.com<br>
+					            	-Lorin Patrick Nicolae: example@example.com
+					            </p>
+					         </div>
+					    </li>
+					    
+					  </ul>  	
+					</div>
+					<!-- ENDS slider content --> 
+				           
+					<!-- slider nav -->
+					<div class="navigator-content">
+					  <div class="navigator-wrapper">
+					        
+					  </div>
+					  <div class="button-next"></div>
+					  <div  class="button-previous"></div>
+					  <!-- REMOVED TILL FIXED <div class="button-control"><span>STOP</span></div> -->
+					</div> 
+					<!-- slider nav -->			
+				          
+				 </div> 
+				<!-- ENDS SLIDER -->
+</html>
