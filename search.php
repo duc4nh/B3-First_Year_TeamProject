3956002e861809a4720f26ee71ae3f4b2d331be5
Karolis Skripka <1karoliskarolis1@gmail.com>
2013-03-14 16:42:33 +0000

    Karolis Skripka added title and links to search results

--- search.php
+++ search.php
@@ -13,26 +13,34 @@
 		$query = "SELECT * FROM `items` WHERE `status` = 1 AND `name` LIKE '%{$search}%' AND `description` LIKE '%{$search}%'";
 		 $data = mysql_query($query); 
 		 echo "<div id='all_items'>"; 
-		 while($info = mysql_fetch_array($data)) 
-		 { 
-			$image = $info['picture'];
-			if(empty($image))
-				$image = 'http://www.worldofchemicals.com/Woclite/tmp/chem/no_image.gif';		
-			else
-				$image = "uploads/".$image;
-		?>
-			<div class="item_in_list">
-				<div class="item_in_list_image">
-					<img height="150" weight="150" alt="<?=$info['name'];?>" src="<?=$image?>">
+		 if(mysql_num_rows($data))
+		 {
+			 while($info = mysql_fetch_array($data)) 
+			 { 
+				$image = $info['picture'];
+				if(empty($image))
+					$image = 'http://www.worldofchemicals.com/Woclite/tmp/chem/no_image.gif';		
+				else
+					$image = "uploads/".$image;
+			?>
+				<div class="item_in_list">
+					<div class="item_in_list_image">
+						<a href="item_page.php?id=<?=$info['item_id']?>"><img height="150" weight="150" alt="<?=$info['name'];?>" src="<?=$image?>"></a>
+					</div>
+					<div class="item_in_list_name">
+						<a href="item_page.php?id=<?=$info['item_id']?>"><b><?=stripslashes(substr($info['name'],0,150));?></b></a>
+					</div>
+					<div class="item_in_list_description">
+						<a href="item_page.php?id=<?=$info['item_id']?>"><?=stripslashes(substr($info['description'],0,150));?></a>
+					</div>
+					<div class="item_in_list_price">
+						<?=stripslashes(substr($info['price'],0,150));?> &#163;
+					</div>				
 				</div>
-				<div class="item_in_list_description">
-					<?=stripslashes(substr($info['description'],0,150));?>
-				</div>
-				<div class="item_in_list_price">
-					<?=stripslashes(substr($info['price'],0,150));?> &#163;
-				</div>				
-			</div>
-		 <? } 
+			 <? } 
+		 } // if
+		 else
+			echo "Nothing found";
 		 echo "</div>"; 
 	} // if
 	?>
