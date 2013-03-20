e24307d58e460666c1b5e62c4bb65db662bd1f2e
Karolis Skripka <1karoliskarolis1@gmail.com>
2013-03-20 21:26:01 +0000

    password encoding added

--- register.php
+++ register.php
@@ -1,14 +1,15 @@
 <!-- header and menu left -->
-<?php include('header_menuleft.php'); ?>
+<?php include_once("functions.php"); 
+include('header_menuleft.php'); ?>
   
   <!-- Main body for page -->
   
 <?php
-$email = $_POST['regemail'];
-$pass1 = $_POST['regpass1'];
-$pass2 = $_POST['regpass2'];
-$firstname = $_POST['refirstname'];
-$lastname = $_POST['relastname'];
+$email = escape_value($_POST['regemail']);
+$pass1 = escape_value($_POST['regpass1']);
+$pass2 = escape_value($_POST['regpass2']);
+$firstname = escape_value($_POST['refirstname']);
+$lastname = escape_value($_POST['relastname']);
 
 
 include('config.php');
@@ -50,7 +51,7 @@ if (!preg_match('/^[a-zA-Z\" "]*$/', $firstname)
       }
       else
       {
-        mysql_query("INSERT into users (email, password, name, last_name , picture) values('$email','$pass1','$firstname','$lastname','$picture')") or die (mysql_error());
+        mysql_query("INSERT into users (email, password, name, last_name , picture) values('$email','".md5($pass1)."','$firstname','$lastname','$picture')") or die (mysql_error());
         echo "<h2>Thank you!!! You have sucessfully registered</h2>";
         echo "Please log in, on the top of the page";
       }
