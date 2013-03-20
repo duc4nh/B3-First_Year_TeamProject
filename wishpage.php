8a3eff769068d04151ec702d4fc0b8532b4181c7
Lorin Nicolae <nicolae.lorin@gmail.com>
2013-03-20 21:53:07 +0000

    Added some details for the fields

--- wishpage.php
+++ wishpage.php
@@ -23,6 +23,19 @@ $query2 = mysql_query("SELECT * FROM wishlist WHERE user_id = '$user_id' ");
               $item_name_wish = $row['name'];
               $owner_id = $row['user_id'];
               
+
+              $item_id = $row['item_id'];
+              $user_id = $row['user_id'];
+              $creation_date = $row['creation_date'];
+              $expiration_date = $row['expiration_date'];
+              $status = $row['status'];
+              $type = $row['type'];
+              $name = $row['name'];
+              $category_id = $row['category_id'];
+              $views = $row['views'];
+
+
+
               $picture = $row['picture'];
   
               if($picture == NULL)
@@ -40,8 +53,16 @@ $query2 = mysql_query("SELECT * FROM wishlist WHERE user_id = '$user_id' ");
              <div class='top_prod_box'></div>
              <div class='center_prod_box'>            
                   <div class='product_title'><a href='item_page.php?id=".$item_id."'>".$name."</a></div>
+
+                 <div class='creation_date'>Created: <span class='creation'>".$creation_date ."</span></div> 
+                 <div class='expiration_date'>Expires: <span class='expiration'>".$expiration_date ."</span></div> 
+
                   <div class='created_by'>User: <span class='user'>".$owner_name." ".$owner_last_name."</span></div>
                  <div class='product_img'><a href='item_page.php?id=".$item_id."'><img border='0' height='94' weight='94'  src='".$picture."'></a></div>
+
+                 <div class='views_product'>Views: <span class='views'>".$views ."</span></div>  
+                 <div class='category_name'>Category: <span class='category'>".$category_name ."</span></div> 
+
             </div>
             <div class='bottom_prod_box'></div>             
 </div>
