6c1f0f587c76ccaa9f22328db307902eddbf0cd4
Lorin Nicolae <nicolae.lorin@gmail.com>
2013-03-20 14:20:13 +0000

    Pictures were added.

--- selling.php
+++ selling.php
@@ -25,6 +25,12 @@ $user_id = $_SESSION['user_id'];
                  $name = $row['name'];
                  $category_id = $row['category_id'];
                  $views = $row['views'];
+                 $picture = $row['picture'];
+  
+                 if($picture == NULL)
+                  $picture='http://www.tiesummit.com/wp-content/uploads/2012/10/noimage.jpg';
+
+
               $query4 = mysql_query("SELECT * FROM users WHERE user_id = '$user_id' ");
               while ($row = mysql_fetch_assoc($query4)) 
               {
@@ -35,7 +41,7 @@ $user_id = $_SESSION['user_id'];
                 {
                   $category_name = $row['category_name'];
                   
-                    if($status == 1 && $type == 2)
+                    if($status == 1 && $type == 2 && $endline_count < 16)
                     {
 $endline_count++;
 echo "
@@ -48,6 +54,7 @@ echo "
                  <div class='expiration_date'>Expires: <span class='expiration'>".$expiration_date ."</span></div>
                  <div class='price_set'>Price: <span class='price'>".$price ."</span></div> 
                  <div class='created_by'>Owner: <span class='user'>".$owner_name." ".$owner_last_name."</span></div>
+                 <div class='product_img'><a href='item_page.php?id=".$item_id."'><img border='0' height='94' weight='94'  src='".$picture."'></a></div>
                  <div class='views_product'>Views: <span class='views'>".$views ."</span></div>  
                  <div class='category_name'>Category: <span class='category'>".$category_name ."</span></div> 
             </div>
@@ -55,9 +62,8 @@ echo "
 </div>
 ";
 
-if($endline_count==4)
+if($endline_count % 4 == 0)
 {
-  $endline_count=0;
   echo "<br>";
 }
                     }
