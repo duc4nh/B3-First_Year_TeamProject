9e6fe4b90a19b8e3c397a03a7c2db6fce4c888eb
Karolis Skripka <1karoliskarolis1@gmail.com>
2013-03-20 23:37:35 +0000

    fixed weight to width

--- index.php
+++ index.php
@@ -89,7 +89,7 @@ echo "
             <div class='center_prod_box'>            
                  <div class='product_title'><a href='item_page.php?id=".$item_id."'>".$name."</a></div>
                 
-                 <div class='product_img'><a href='item_page.php?id=".$item_id."'><img border='0' height='94' weight='94'  src='".$picture."'></a></div>
+                 <div class='product_img'><a href='item_page.php?id=".$item_id."'><img border='0' height='94' width='94'  src='".$picture."'></a></div>
                  <div class='prod_price'><span class='price'>£".$price."</span></div>                        
             </div>
             <div class='bottom_prod_box'></div>             
@@ -130,7 +130,7 @@ echo "
             <div class='center_prod_box'>            
                  <div class='product_title'><a href='item_page.php?id=".$item_id."'>".$name."</a></div>
                 
-                 <div class='product_img'><a href='item_page.php?id=".$item_id."'><img border='0' height='94' weight='94'  src='".$picture."'></a></div>
+                 <div class='product_img'><a href='item_page.php?id=".$item_id."'><img border='0' height='94' width='94'  src='".$picture."'></a></div>
                  <div class='prod_price'><span class='price'>£".$price."</span></div>                        
             </div>
             <div class='bottom_prod_box'></div>             
