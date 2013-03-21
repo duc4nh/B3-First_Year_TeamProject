55101b4caf6a4fa7f6e9a784f0183e8f14b504cf
Karolis Skripka <1karoliskarolis1@gmail.com>
2013-03-21 00:54:13 +0000

    added full deletation from all tables

--- delete_item.php
+++ delete_item.php
@@ -2,9 +2,11 @@
 <?php include('header_menuleft.php'); ?>  
 <!-- Main body for page -->
 <?
-
-$item_id = $_GET['id'];
+include_once("functions.php");
+$item_id = escape_value($_GET['id']);
 mysql_query("DELETE FROM `items` WHERE `items`.`item_id` = $item_id");
+mysql_query("DELETE FROM `whishlist` WHERE `item_id` = $item_id");
+mysql_query("DELETE FROM `Trade` WHERE `bidded_item_id` = $item_id");
 echo "The item has been deleted. <a href='index.php'>Click here </a> to go back to Home Page!";
 
 ?>
