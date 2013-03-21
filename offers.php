8866c3519a9d968daaca4ed9e14316c4e9f2d9c9
vladut <berianuvlad@gmail.com>
2013-03-21 05:07:22 +0000

    enabled the buttons accept / decline offers

--- offers.php
+++ offers.php
@@ -1,6 +1,43 @@
 <?php
 session_start();
 include('config.php');
+
+$answer = $_GET['id'];
+$offer_item_id = $_GET['idq'];
+$offer_trade_id = $_GET['idw'];
+$offer_trade_name = $_GET['ide'];
+
+echo $offer_item_id;
+echo $offer_trade_id;
+echo $offer_trade_name;
+if($answer == 'accept')
+{
+if($offer_trade_name == '-')
+{
+  mysql_query("UPDATE `items` 
+               SET `status`='0'
+         WHERE `item_id`='$offer_item_id'");
+  mysql_query("DELETE FROM `Trade` WHERE `bidded_item_id` = $offer_trade_id AND `item_id` = $offer_item_id");
+}
+else
+ { mysql_query("UPDATE `items` 
+               SET `status`='0'
+         WHERE `item_id`='$offer_item_id'");
+mysql_query("UPDATE `items` 
+               SET `status`='0'
+         WHERE  `item_id`='$offer_trade_id'");
+  mysql_query("DELETE FROM `Trade` WHERE `bidded_item_id` = $offer_trade_id AND `item_id` = $offer_item_id");
+
+ }
+ header("Location: offers.php");
+}
+if($answer == 'decline')
+  {
+    mysql_query("DELETE FROM `Trade` WHERE `bidded_item_id` = $offer_trade_id AND `item_id` = $offer_item_id");
+    header("Location: offers.php");
+  }
+
+
 $user_id = $_SESSION['user_id'];
 $check = mysql_query("SELECT * FROM Trade WHERE owner_id ='$user_id' OR bidder_id ='$user_id'");
 while($check_row = mysql_fetch_assoc($check))
@@ -10,7 +47,6 @@ while($check_row = mysql_fetch_assoc($check))
   elseif ($user_id == $check_row['bidder_id']) 
     mysql_query("UPDATE Trade SET bidder_view='1' WHERE bidder_id='$user_id'");
 }
-
 include('header_menuleft.php'); 
 
 echo " 
@@ -24,6 +60,8 @@ echo "
     <th id='title_table'><h5>Date</h5></th>
     <th id='title_table'><h5>Answer</h5></th>
 </tr>";
+ 
+  
 
      $query3 = mysql_query("SELECT * FROM Trade WHERE owner_id = '$user_id' OR bidder_id='$user_id'");
      while ($row = mysql_fetch_assoc($query3)) 
@@ -36,24 +74,26 @@ echo "
        $date = $row['date'];
        $bidder_price = $row['price'];
 
-
+       
        $query4 = mysql_query("SELECT * FROM items WHERE item_id = '$item_id' ");
        while ($row = mysql_fetch_assoc($query4)) 
        {    
          $item_name = $row['name'];
+         $status = $row['status'];
        } 
-
+      
        $query5 = mysql_query("SELECT * FROM items WHERE item_id = '$bidder_item_id' ");
-                        echo $bidder_item_id;
-if($bidder_item_id == 0) 
+                        
+       if($bidder_item_id == 0) 
          $bidder_item_name = "-";
-         else {
-       while ($row = mysql_fetch_assoc($query5)) 
-       { 
-         
-
-         $bidder_item_name = $row['name'];
-       }  }
+       else 
+       {
+         while ($row = mysql_fetch_assoc($query5)) 
+         { 
+           $bidder_item_name = $row['name'];
+         }  
+       }
+    
        $query6 = mysql_query("SELECT * FROM users WHERE user_id = '$owner_id' ");
        while ($row = mysql_fetch_assoc($query6)) 
        {    
@@ -64,17 +104,21 @@ if($bidder_item_id == 0)
        {    
          $bidder_name = $row['name'];
        }   
-
-        echo "<tr id='item_table'>
+        if($status != 0)
+        {echo "<tr id='item_table'>
               <td> <a href='item_page.php?id=".$item_id."'>".$item_name."</a></td>
               <td> ".$owner_name ."</td>
               <td> ".$bidder_item_name."</td>
               <td> ".$bidder_price."</a></td>
               <td> ".$bidder_name."</a></td>
-              <td> ".$date."</a></td>
-              <td> <a href='answer.php?id=accept'><button>Accept offer</button></a>
-                             <a href='answer.php?id=decline'><button>Decline offer</button></a></td>
-              </tr>"; 
+              <td> ".$date."</a></td>";
+           if($_SESSION['user_id'] == $owner_id)
+             {echo "<td> <a href='offers.php?id=accept&idq=".$item_id."&idw=".$bidder_item_id."&ide=".$bidder_item_name."'><button>Accept offer</button></a>
+                             <a href='offers.php?id=decline&idq=".$item_id."&idw=".$bidder_item_id."&ide=".$bidder_item_name."'><button>Decline offer</button></a></td>
+              </tr>"; }
+           else
+             echo "<td></td></tr>";
+          }
      }
 echo "</table>";
 include('footer.php'); 
