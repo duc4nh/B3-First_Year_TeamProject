35ae785c655411610bed0ecbacfc8b13fbbdf2ec
Karolis Skripka <1karoliskarolis1@gmail.com>
2013-03-22 00:53:31 +0000

    Added divs layout

--- wishpage.php
+++ wishpage.php
@@ -54,7 +54,7 @@ $query2 = mysql_query("SELECT * FROM wishlist WHERE user_id = '$user_id' ");
                 echo "
            <div class='prod_box'>
              <div class='top_prod_box'></div>
-             <div class='center_prod_box'>            
+             <div class='center_prod_box blocks'>            
                  <div class='product_title'><a href='item_page.php?id=".$item_id."'>".$name."</a></div>
 		 <div class='product_img'><a href='item_page.php?id=".$item_id."'><img border='0' height='94' width='94'  src='".$picture."'></a></div>
                  <div class='creation_date'>Created: <span class='creation'>".$creation_date ."</span></div> 
@@ -68,10 +68,6 @@ $query2 = mysql_query("SELECT * FROM wishlist WHERE user_id = '$user_id' ");
 </div>
 ";
 
-if($endline_count % 4 == 0)
-{
-  echo "<br>";
-}
               }
          
             }
@@ -82,7 +78,46 @@ if ($item_found != 1)
 ?>
 
 </div>
+<script>
+var currentTallest = 0,
+     currentRowStart = 0,
+     rowDivs = new Array(),
+     $el,
+     topPosition = 0;
 
+ $('.blocks').each(function() {
+
+   $el = $(this);
+   topPostion = $el.position().top;
+   
+   if (currentRowStart != topPostion) {
+
+     // we just came to a new row.  Set all the heights on the completed row
+     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+       rowDivs[currentDiv].height(currentTallest);
+     }
+
+     // set the variables for the new row
+     rowDivs.length = 0; // empty the array
+     currentRowStart = topPostion;
+     currentTallest = $el.height();
+     rowDivs.push($el);
+
+   } else {
+
+     // another div on the current row.  Add it to the list and check if it's taller
+     rowDivs.push($el);
+     currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+
+  }
+   
+  // do the last row
+   for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+     rowDivs[currentDiv].height(currentTallest);
+   }
+   
+ })
+</script>
 <?php
 
 include('footer.php'); 
